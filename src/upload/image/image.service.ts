efaec6c472878bf884399a1da4ab3f05e2d672d4
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2026-01-18 12:28:05 +0530

    Upload Directory Error Fixed

--- src/upload/image/image.service.ts
+++ src/upload/image/image.service.ts
@@ -1,13 +1,17 @@
 import sharp from "sharp";
 import path from "path";
 import crypto from "crypto"
+import fs from "fs/promises"
 
-const uploadPath = path.join(process.cwd(), "public/uploads/images")
+const uploadPath = path.join(process.cwd(), "public","uploads","images")
 
 export const processAndSaveImage = async (file: Express.Multer.File) => {
   const fileName = `${crypto.randomUUID()}.webp`
   const filePath = path.join(uploadPath, fileName)
   const imageUrl = `${process.env.BASE_URL}/uploads/images/${fileName}`
+
+  // Ensure Directory Exists
+  await fs.mkdir(uploadPath, { recursive: true })
 
   const info = await sharp(file.buffer)
     .resize({ width: 1024, withoutEnlargement: true })
