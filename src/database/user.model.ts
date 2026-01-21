732d8cff99b10f81fbb37be4585ab20154708fb6
home-ashif <ashifshaikh9a@gmail.com>
2026-01-21 16:12:23 +0530

    User Model Added

---
+++ src/database/user.model.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  displayName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false
+  },
+  role: {
+    type: String,
+    enum: ["user", "manager", "admin"],
+    default: "user"
+  },
+  image: {
+    type: String,
+    require: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  isBanned:{
+    type:Boolean,
+    default:false
+  },
+  deletedAt:{
+    type:Date
+  },
+  deleteBy:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+  },
+},{timestamps:true})
+
+export const User = mongoose.model("User",userSchema)
+
