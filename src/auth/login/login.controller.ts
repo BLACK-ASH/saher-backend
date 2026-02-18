e2539652ee1cde415210c52d7c54749dd752f7c0
home-ashif <ashifshaikh9a@gmail.com>
2026-02-18 16:59:58 +0530

    Login Added

--- src/auth/login/login.controller.ts
+++ src/auth/login/login.controller.ts
@@ -1,31 +1,42 @@
 import { Request, Response } from "express"
 import { User } from "../../database/user.model.js"
-import { Account } from "../../database/account.model.js"
+import { comparePassword } from "../../libs/utils/password-hash.js"
+import { generateToken } from "../../libs/utils/jwt-token.js"
 
 export const loginController = async (req: Request, res: Response) => {
   const { email, password } = req.body
 
   try {
+    const token = {
+      accessToken: req.cookies?.saher_access_token,
+      refreshToken: req.cookies?.saher_refresh_token
+    }
+
+    if (token.accessToken && token.refreshToken) {
+      return res.status(200).json({ success: true, message: "Already Login.", data: token })
+    }
+
     const user = await User.findOne({ email })
 
     if (!user) {
       return res.status(404).json({ success: false, message: "User Not Found.", data: null })
     }
 
-    const userAccount = await Account.findOne({ user: user._id })
+    const matchPassword = await comparePassword(password, user.password!)
 
-    if (!userAccount) {
-      return res.status(404).json({ success: false, message: "User Account Not Found.", data: null })
+    if (!matchPassword) {
+      return res.status(403).json({ success: false, message: "Invalid credentials", data: null })
     }
 
-    if (!userAccount.password === password) {
-      return res.status(400).json({ success: false, message: "Invalid Credentials.", data: null })
-    }
+    const { accessToken, refreshToken } = generateToken({ id: user._id.toString(), name: user.name!, role: user.role })
 
-    return res.status(200).json({ success: true, message: "Login Succesfully.", data: "token" })
+    res.cookie("saher_access_token", accessToken, { maxAge: 604800000, httpOnly: true, secure: true, sameSite: "none" })
+    res.cookie("saher_refresh_token", refreshToken, { maxAge: 7776000000, httpOnly: true, secure: true, sameSite: "none" })
+
+    return res.status(200).json({ success: true, message: "login succesfully.", data: { accessToken, refreshToken } })
 
   } catch (error) {
     console.error(error)
-    return res.status(500).json("Internal Server Error")
+    return res.status(500).json({ success: false, message: "Internal Server Error", data: error })
   }
 }
---
+++ src/libs/utils/jwt-token.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken"
+
+type TokenInput = {
+  id: string,
+  name: string,
+  role: string
+}
+
+export const generateToken = (data: TokenInput) => {
+  const accessToken = jwt.sign(data, process.env.JWT_ACCESS_SECRET!, { algorithm: "HS384", expiresIn: "7d" })
+  const refreshToken = jwt.sign(data, process.env.JWT_REFRESH_SECRET!, { algorithm: "HS512", expiresIn: "90d" })
+  return { accessToken, refreshToken }
+}
+
+export const verifyAccessToken = async (accessToken: string) => {
+  const data = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET!, { algorithms: ["HS384"] })
+  return data
+}
+
+export const verifyRefreshToken = async (refreshToken: string) => {
+  const data = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET!, { algorithms: ["HS512"] })
+  return data
+}
