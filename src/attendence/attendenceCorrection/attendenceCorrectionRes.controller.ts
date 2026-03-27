8d482aea4fae3ac44aa7d5d15d95afcf871be1f0
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-03-27 20:49:51 +0530

    Added Some changes in the Attendence Correction module and other minor changes as well

--- src/attendence/attendenceCorrection/attendenceCorrectionRes.controller.ts
+++ src/attendence/attendenceCorrection/attendenceCorrectionRes.controller.ts
@@ -8,13 +8,14 @@ import { log } from "node:console"
 export const attendenceCorrectionResController = async (req: Request, res: Response) => {
     const { userID, status , dateForCorrection} = req.body
 
+    const finalStatus = status.toLowerCase()
     const record = await AttendenceCorrection.findOne({ requestedBy: userID , dateForCorrection : dateForCorrection})
 
     if (!record) {
         return res.status(400).json({ message: "Record not found " })
     }
 
-    if(status === "Approved"){
+    if(finalStatus === "approved"){
         const recordToEdit = await Attendence.findOne({ userID : userID , Date : record.dateForCorrection  })
 
         if(!recordToEdit){
@@ -27,26 +28,29 @@ export const attendenceCorrectionResController = async (req: Request, res: Respo
             recordToEdit.status = record.demandsToBe
             recordToEdit.isLate = record.isLate
             await recordToEdit.save()
+            await record.deleteOne()
             return res.status(200).json({message:"The attendence correction request was approved and has been updated successfully" , success: true })
-
+ 
         } catch (error) {
             console.log(error)
             return res.status(400).json({message : "THere is some error"  , success : false})
         }
         
 
-        return res.status(200).json({message:"you have approved the request the attendence has been updated"})
+        
     }
 
-    if (status === "Rejected"){
+    if (finalStatus=== "rejected"){
         await record.deleteOne()
         return res.status(200).json({message:"You have rejected the request and the record has been deleted "})
     }
 
-    if (status === "Hold"){
+    if (finalStatus === "hold"){
         return res.status(200).json({message : "You have put the request on hold"})
     }
-   
+    else{
+        return res.status(404).json({message : "Please enter a valid status for attendence " , success : false})
+    }
 
     
 }
