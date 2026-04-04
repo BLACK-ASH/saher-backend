77f07f52e56fa3eb5e0c95444b12cef8d9466bb3
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-04-04 20:02:16 +0530

    feat(attendance):Added the feature of changing status depending upon no. of Hours and employee type

--- src/attendance/mark/check-out.controller.ts
+++ src/attendance/mark/check-out.controller.ts
@@ -3,42 +3,6 @@ import { Attendance } from "../../database/attendance.model.js"
 import { ApiError } from "../../libs/class/api-error.js";
 import { timeDifference } from "../../libs/utils/time-difference.js";
 
-export const checkOutController = async (req: Request, res: Response) => {
-  const user = req.user
-  const now = new Date()
-
-  const attendance = await Attendance.findOne({
-    user: user?.id,
-    date: now.toLocaleDateString("en-CA"),
-    inTime: { $ne: null }
-  })
-
-  // If User Is Not Check In
-  if (!attendance) throw new ApiError(400, "You Have Not Checked Out Today.")
-  // If User Is Already Check Out
-  if (attendance?.outTime) throw new ApiError(400, "You Have Already Checked Out Today")
-
-  // Calculate The Work Hour And Status
-  const workHours = timeDifference(attendance.inTime as Date, now).hours
-  if (!workHours) throw new ApiError(400, "Work Hours Is Not Valid.")
-  const status = workHours === 0 ? "absent" : workHours > 8 ? "present" : "half-day"
-
-  attendance.outTime = now
-  attendance.status = status
-  attendance.workHours = workHours
-
-  await attendance.save()
-  
-  return res.status(200).json({
-    message: "Checked out successfully",
-    success: true,
-    data: attendance
-  })
-
-}
-
-
-
 // export const checkOutController = async (req: Request, res: Response) => {
 //   const user = req.user
 //   const now = new Date()
@@ -55,15 +19,9 @@ export const checkOutController = async (req: Request, res: Response) => {
 //   if (attendance?.outTime) throw new ApiError(400, "You Have Already Checked Out Today")
 
 //   // Calculate The Work Hour And Status
-//   let expectedWorkHours 
-//   if(req.user.type === "full-time"){
-    // expectedWorkHours = 8 
-    // }else if(req.user.type === "part-time") {
-    //  expectedWorkHours = 4 
-    // }
 //   const workHours = timeDifference(attendance.inTime as Date, now).hours
 //   if (!workHours) throw new ApiError(400, "Work Hours Is Not Valid.")
-//   const status = workHours === 0 ? "absent" : workHours > expectedWorkHours ? "present" : "half-day"
+//   const status = workHours === 0 ? "absent" : workHours > 8 ? "present" : "half-day"
 
 //   attendance.outTime = now
 //   attendance.status = status
@@ -78,3 +36,45 @@ export const checkOutController = async (req: Request, res: Response) => {
 //   })
 
 // }
+
+
+
+export const checkOutController = async (req: Request, res: Response) => {
+  const user = req.user
+  const now = new Date()
+
+  const attendance = await Attendance.findOne({
+    user: user?.id,
+    date: now.toLocaleDateString("en-CA"),
+    inTime: { $ne: null }
+  })
+
+  // If User Is Not Check In
+  if (!attendance) throw new ApiError(400, "You Have Not Checked Out Today.")
+  // If User Is Already Check Out
+  if (attendance?.outTime) throw new ApiError(400, "You Have Already Checked Out Today")
+
+  // Calculate The Work Hour And Status
+  let expectedWorkHours 
+  if(req.user.employeeType === "full-time"){
+    expectedWorkHours = 8 
+    }else if(req.user.employeeType === "part-time") {
+     expectedWorkHours = 4 
+    }
+  const workHours = timeDifference(attendance.inTime as Date, now).hours
+  if (!workHours) throw new ApiError(400, "Work Hours Is Not Valid.")
+  const status = workHours === 0 ? "absent" : workHours > expectedWorkHours ? "present" : "half-day"
+
+  attendance.outTime = now
+  attendance.status = status
+  attendance.workHours = workHours
+
+  await attendance.save()
+  
+  return res.status(200).json({
+    message: "Checked out successfully",
+    success: true,
+    data: attendance
+  })
+
+}
