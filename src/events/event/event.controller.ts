f9f2aed6394695168990ed07baf223c3d3d3c56c
arshadshaikh1611206-art <arshadshaikh1611206@gamil.com>
2026-04-04 20:35:09 +0530

    refactor (try-catch removed)

--- src/events/event/event.controller.ts
+++ src/events/event/event.controller.ts
@@ -1,30 +1,34 @@
 import { Request, Response } from "express";
 import { Event } from "../../database/event.model.js";
+import { ApiError } from "../../libs/class/api-error.js";
+import { success } from "zod";
 
 //Add an event
 export const addEvent = async (req: Request, res: Response) => {
-  try {
-    const newEvent = await Event.create(req.body);
-    res.json(newEvent);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to add an event" });
-  }
+  const newEvent = await Event.create(req.body);
+  if (!newEvent) throw new ApiError(500, "Failed to add an event");
+  return res.status(200).json({
+    success: true,
+    message: "Event added successfully.",
+    data: newEvent
+  });
 };
 
 //Delete an event
 export const deleteEvent = async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    await Event.findByIdAndDelete(id);
-    res.json({ message: "Event has been deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to delete an event" });
-  }
+  const id = req.params.id;
+  const deleted = await Event.findByIdAndDelete(id);
+  if (!deleted) throw new ApiError(500, "Failed to delete an Event")
+  return res.status(200).json({
+    success: true,
+    message: "Event has been deleted successfully",
+    data: null
+  });
 };
 
 //Edit an event
 export const editEvent = async (req: Request, res: Response) => {
-  try {
+  
     const updatedEvent = await Event.findByIdAndUpdate(
       req.params.id,
       req.body,
@@ -36,9 +40,10 @@ export const editEvent = async (req: Request, res: Response) => {
     if (!updatedEvent) {
       return res.status(404).json({ error: "Event not found" });
     }
-
-    res.json(updatedEvent);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to update event" });
-  }
+    return res.status(500).json({
+      success: true,
+      message: "Event Updated successfully",
+      data: updatedEvent
+    });
+    // res.status(500).json({ error: "Failed to update event" });
 };
