0fc4c88aaa8af5b91a3133aa934b63fcafa0b6d2
BLACK-ASH <ashifshaikh9a@gmail.com>
2026-04-04 14:57:37 +0530

    Refactor (Code changes in events.routes)

--- src/events/events.routes.ts
+++ src/events/events.routes.ts
@@ -1,10 +1,10 @@
-import express from "express";
+import {Router , Request, Response} from "express";
 import { addEvent, deleteEvent, editEvent } from "./event/event.controller.js";
 
-const router = express.Router();
+const router = Router();
 router.post("/", addEvent);
-router.get("/", (req, res) => {
-  res.send("Programs API is working");
+router.get("/", (req: Request, res:Response) => {
+  return res.status(200).json({ message: "This Is A EventRouter Page" })
 });
 router.delete("/:id",deleteEvent)
 router.patch("/:id", editEvent)
