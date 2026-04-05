85d8c3893a1de106e0124758db6ba296feb00ace
Harsh Salvi <harshsalvi250@gmail.com>
2026-04-05 14:57:33 +0530

    feat (Added workshop files)

---
+++ src/database/workshop.model.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import { required } from "zod/mini";
+
+const workshopSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    startDate: {
+      type: Date,
+      required: true,
+    },
+
+    endDate: {
+        type: Date,
+        required:true,
+    },
+
+    createdBy: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export type workshopType = mongoose.InferSchemaType<typeof workshopSchema>;
+export const Workshop = mongoose.model<workshopType>("Workshop", workshopSchema);
