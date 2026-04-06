76ce28174791f341902ca4addba215615ef6abf0
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-04-06 19:26:17 +0530

    Created the Schema , tested all the notification end points , result positive

--- src/notification/notification.controllers.ts
+++ src/notification/notification.controllers.ts
@@ -2,12 +2,12 @@ import { Request , Response } from "express";
 import { ApiError } from "../libs/class/api-error.js";
 import { Notification } from "../database/notification.model.js";
 
-
+//Create a new Notification
 export const createNotificationController = async ( req:Request , res:Response)=>{
     const user = req.user 
 
     //Checking if the user is Admin 
-    if(user?.role !== "admin" || "manager"){
+    if(user?.role !== "admin" && "manager"){
         throw new ApiError(400,"Only Admins and managers are allowed to create a notification")
     }
 
@@ -22,7 +22,7 @@ export const createNotificationController = async ( req:Request , res:Response)=
     return res.status(200).json({message:`The notification with the title ${title} has been successfully created` , success : true })
 } 
 
-
+//Get the most recent Notification 
 export const getLatestNotificationController = async(req:Request , res:Response)=>{
     
     const countNotification = await Notification.countDocuments()
@@ -35,7 +35,7 @@ export const getLatestNotificationController = async(req:Request , res:Response)
 
 }
 
-
+//Get all the Notification 
 export const getAlltNotificationController = async(req:Request , res:Response)=>{
     
     const countNotification = await Notification.countDocuments()
