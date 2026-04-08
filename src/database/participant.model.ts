e85b1c184ddd8529ddf13cfba82729c3ec631503
Harsh Salvi <harshsalvi250@gmail.com>
2026-04-08 14:58:02 +0530

    feat (Added participant model)

---
+++ src/database/participant.model.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { required } from "zod/mini";
+
+const participantSchema = new mongoose.Schema (
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+
+        age: {
+            type: String,
+            required: true,
+        },
+
+        gender: {
+            type: String,
+            required: true,
+        },
+    },
+    {timestamps: true},
+);
+
+export type participantType = mongoose.InferSchemaType<typeof participantSchema>;
+export const Participant = mongoose.model<participantType>("Participant", participantSchema);
