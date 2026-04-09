f6e57745b3d62846a38cd36134495e420532eab4
arshadshaikh1611206-art <arshadshaikh1611206@gamil.com>
2026-04-09 18:21:59 +0530

    fix

--- src/events/participant/participant.schema.ts
+++ src/events/participant/participant.schema.ts
@@ -6,14 +6,14 @@ export const createParticipantSchema = z.object({
   workshopId: objectId,
   name: z.string().min(5).max(30),
   age: z.string().min(1).max(3),
-  gender: z.string().min(5).max(20),
+  gender: z.string().min(3).max(20),
 })
   .strict();
 
 export const updatedParticipantSchema = z.object({
   name: z.string().min(5).max(30).optional(),
   age: z.string().min(1).max(3).optional(),
-  gender: z.string().min(5).max(20).optional(),
+  gender: z.string().min(3).max(20).optional(),
 })
   .strict();
 
