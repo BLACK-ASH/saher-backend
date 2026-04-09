93a6f00c7f866fe7430b3690d9635637077589d6
arshadshaikh1611206-art <arshadshaikh1611206@gamil.com>
2026-04-09 20:26:00 +0530

    feat (sessionAttendance) added a validation for session attendance

--- src/events/sessionAttendance/sessionAttendance.controller.ts
+++ src/events/sessionAttendance/sessionAttendance.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { SessionAttendance } from "../../database/sessionAttendance.model.js";
 import { string, success } from "zod";
 import { ApiError } from "../../libs/class/api-error.js";
+import { Session } from "../../database/session.model.js";
+import { Participant } from "../../database/participant.model.js";
 
 export const markAttendance = async (req: Request, res: Response) => {
 
@@ -12,10 +14,35 @@ export const markAttendance = async (req: Request, res: Response) => {
     // conditon to check we have both sessionid and participantId if one them is not throw error
     if (!sessionId || !participantIds?.length) throw new ApiError(400, "sessionId and participantIds is required");
 
-    const notExist = await SessionAttendance.findById()
+    // if the session of the follwing ID not exist throw error 
+    const sessionExist = await Session.findById(sessionId);
+    if (!sessionExist) throw new ApiError(400, "Session not found");
+
+    // after finding the seesion get valid participants of the same workshop
+    const validParticipants = await Participant.find({
+        _id: { $in: participantIds },
+        workshopId: sessionExist.workshopID,
+    }).select("_id");
+    if (validParticipants.length === 0) {
+        throw new ApiError(400, "No participants found for this workshop");
+    }
+
+    // converting object into string
+    const validIds = validParticipants.map(p => p._id.toString());
+
+    const inValidIds = participantIds.filter(inId => !validIds.includes(inId))
+    if (inValidIds.length > 0) {
+        throw new ApiError(
+            400,
+            `These participants are not part of this workshop: ${inValidIds.join(", ")}`
+        );
+    }
+
+    // only present participants will be shown
+    const filteredIds = participantIds.filter(id => validIds.includes(id));
 
     // Convert array of IDs → array of database records
-    const records = participantIds.map(participantId => ({ sessionId, participantId }))
+    const records = filteredIds.map(participantId => ({ sessionId, participantId }))
 
     // Inserting the data and preventing duplicate data
     await SessionAttendance.insertMany(records, {
