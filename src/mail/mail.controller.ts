04d473390d6ff21f9849be59e52b92f38d6abe2a
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-04-11 11:38:45 +0530

    refactor(mail):Paid attention to the res , whether it is having success , data and msg or not

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -10,19 +10,16 @@ export const inboxController = async(req:Request , res:Response)=>{
     const record = await Mail.find({to:user?.id}).lean().sort({createdAt:-1})
     const length = record.length
     if(length === 0 ){
-        return res.status(200).json({message:"There are no mails for you" , count : 0 })
+        return res.status(200).json({message:"There are no mails for you" , count : 0 , data : null , success : true })
     }
 
     return res.status(200).json({message:"The mails in your inbox are " , data : record , count : length , success : true  })
 }
 
-
-
 export const sendMailController = async(req : Request , res:Response)=>{
     const user = req.user 
 
     const { receiversID , subject , body } = req.body
-
     
     if(!Array.isArray(receiversID) || receiversID.length === 0){
         throw new ApiError(400 , " ReceiversID must be a non empty array")
@@ -44,10 +41,8 @@ export const sendMailController = async(req : Request , res:Response)=>{
 
     await Mail.insertMany(mails)
 
-    return res.status(201).json({message:`The mail has been sent to ${receiversID} ` , count : receiversID.length ,  success : true })
-
+    return res.status(201).json({message:`The mail has been sent to ${receiversID} ` , count : receiversID.length ,  success : true , data : null})
 }
-
 
 export const outboxController = async(req:Request , res:Response)=>{
     const user = req.user 
@@ -55,36 +50,8 @@ export const outboxController = async(req:Request , res:Response)=>{
     const record = await Mail.find({from:user?.id})
 
     if(record.length === 0){
-        return res.status(200).json({message:"There are no mails for you" , count : 0 })
-
+        return res.status(200).json({message:"There are no mails for you" , count : 0 , data : null , success : true })
     }
 
-    return res.status(200).json({message:"The mails sent by you are " , data : record  , count: record.length})
+    return res.status(200).json({message:"The mails sent by you are " , data : record  , count: record.length , success : true })
 }
-
-// export const sendMailToAllController = async(req:Request , res:Response)=>{
-//     const user = req.user 
-
-//     if(user?.role !== "admin" &&  user?.role !== "manager"){
-//         throw new ApiError(403,"Onlu Admins and managers  are allowed to send mails to everyone at once ")
-//     }
-
-//     const { subject , body } = req.body 
-
-//     const users = await User.find({ _id :{$ne:user?.id}})
-
-//     if(users.length === 0){
-//         throw new ApiError(404,"No Users were found to send this mail")
-//     }
-
-//     const mails = users.map( oneUser =>({
-//         from : user?.id,
-//         to : oneUser._id ,
-//         subject : subject , 
-//         body : body
-//     }))
-
-//     await Mail.insertMany(mails)
-
-//     return res.status(200).json({message:`The mail with subject ${subject} has been sent to ${users.length}` , count : mails.length ,success:true })
-// }
