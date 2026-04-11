65ebfa1404018d52f250d1377267f5aeb8c53714
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-04-11 11:10:11 +0530

    fix(notification):Fixed the route

--- src/notification/notification.routes.ts
+++ src/notification/notification.routes.ts
@@ -8,7 +8,7 @@ const notificationRouter = Router()
 
 notificationRouter.get("/" , getLatestNotificationController)
 notificationRouter.get("/all" , getAlltNotificationController)
-notificationRouter.post("/create" , authorize("write" , "notification") , validate(createNotificationSchema),  createNotificationController)
+notificationRouter.post("/create/:id" , authorize("write" , "notification") , validate(createNotificationSchema),  createNotificationController)
 notificationRouter.put("/update/:id" , authorize("update" ,"notification") , validate(updateNotificationSchema) , updateNotificationController)
 notificationRouter.delete("/delete" , authorize("delete","notification"),deleteNotificationController)
 
