80fb1d2166ba7bd523183ab1ab169896159c3841
Harsh Salvi <harshsalvi250@gmail.com>
2026-04-12 16:13:47 +0530

    fix (Fixed data relation in session controller)

--- src/events/session/session.controller.ts
+++ src/events/session/session.controller.ts
@@ -3,31 +3,33 @@ import { Participant } from "../../database/participant.model.js";
 import { SessionAttendance } from "../../database/sessionAttendance.model.js";
 import { Session } from "../../database/session.model.js";
 import { ApiError } from "../../libs/class/api-error.js";
-import { success } from "zod";
+import { Workshop } from "../../database/workshop.model.js";
 
 //Add a session
 export const addSession = async (req: Request, res: Response) => {
   const { workshopId } = req.body;
-
   if (!workshopId) {
     throw new ApiError(400, "workshopId is required");
+  }
+  const workshop = await Workshop.findById(workshopId);
+
+  if (!workshop) {
+    throw new ApiError(404, "Workshop not found");
   }
 
   const newSession = await Session.create(req.body);
 
-  if (!newSession) {
-    throw new ApiError(500, "Failed to add a Session");
-  }
-
   const participants = await Participant.find({ workshopId });
 
-  await SessionAttendance.insertMany(
-    participants.map((p) => ({
-      sessionId: newSession._id,
-      participantId: p._id,
-      status: "absent",
-    })),
-  );
+  if (participants.length > 0) {
+    await SessionAttendance.insertMany(
+      participants.map((p) => ({
+        sessionId: newSession._id,
+        participantId: p._id,
+        status: "absent",
+      })),
+    );
+  }
 
   return res.status(200).json({
     success: true,
