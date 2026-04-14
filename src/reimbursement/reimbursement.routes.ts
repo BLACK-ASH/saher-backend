d6d7f5a55235360fa7eece290a6b9f7763fcd6a6
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-04-14 12:29:09 +0530

    refactor(reimbursement):Added the validation for the schema by using the validate method

--- src/reimbursement/reimbursement.routes.ts
+++ src/reimbursement/reimbursement.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createReimbursement, getAllReimbursements, getMyReimbursements, reviewReimbursement } from "./reimbursement.controller.js";
 import { authorize } from "../permission/authorize.js";
+import { reimbursementReviewSchema, reimbursementSchema } from "./reimbursement.schema.js";
+import { validate } from "../libs/middleware/validate-zod-schema.js";
 
 export const reimbursementRouter = Router() 
 
 
 reimbursementRouter.get("/",getMyReimbursements)
 reimbursementRouter.get("/all",getAllReimbursements)
-reimbursementRouter.post("/create",authorize("write","reimbursement"),createReimbursement)
-reimbursementRouter.put("/review" , authorize("update","reimbursement"),  reviewReimbursement)
+reimbursementRouter.post("/create",authorize("write","reimbursement"),validate(reimbursementSchema),createReimbursement)
+reimbursementRouter.put("/review" , authorize("update","reimbursement"),validate(reimbursementReviewSchema),  reviewReimbursement)
