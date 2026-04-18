1a2cb53f7315980743111f8a8bba675de973e09f
arshadshaikh1611206-art <arshadshaikh1611206@gamil.com>
2026-04-18 17:55:53 +0530

    refactor(event): workshop participants delete changes and participants schema

--- src/events/workshop/workshop-participant.controller.ts
+++ src/events/workshop/workshop-participant.controller.ts
@@ -30,7 +30,7 @@ export const removeParticipantFromWorkshop = async (req: Request, res: Response)
   const workshop = await Workshop.findByIdAndUpdate(
     workshopId,
     {
-      $pull: { participants: participantId },
+      $pull: { participants: { $in: participantId } },
     },
     { new: true },
   );
