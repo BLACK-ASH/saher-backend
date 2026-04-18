1764b1eb6037f2f22cb9680648dc46dba9f14777
Ashif Shaikh <ashifshaikh9a@gmail.com>
2026-04-18 11:13:24 +0530

    fix(admin): admin can change user details

--- src/admin/user/schema.ts
+++ src/admin/user/schema.ts
@@ -1,5 +1,5 @@
 import { userSchema } from '../account/schema.js';
 import z from 'zod';
 
-export const userUpdateSchema = userSchema.pick({ displayName: true, image: true }).partial();
+export const userUpdateSchema = userSchema.partial();
 export type UserUpdate = z.infer<typeof userUpdateSchema>;
