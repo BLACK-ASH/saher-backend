b10ca824b14e97c475acda15277dd5f90d50aca2
sachindjaiswal <sachin.dharmendrajaiswal@gmail.com>
2026-05-17 16:34:35 +0530

    fix(notification): fixed the scope of the notification in createAttendanceCorrection

--- src/attendance/correction/create-correction.ts
+++ src/attendance/correction/create-correction.ts
@@ -64,7 +64,7 @@ export const createAttendanceCorrectionController = async (req: Request, res: Re
   const notificationTitle = 'Receieved New Attendance Correction request';
   const notificationDesc = `A new Attendance Correction Request for the date ${attendance.date} has been submitted `;
   await NotificationService.role.success('admin', notificationTitle, notificationDesc);
-  await NotificationService.role.success('admin', notificationTitle, notificationDesc);
+  await NotificationService.role.success('manager', notificationTitle, notificationDesc);
   return ApiResponse.success(res, {
     message: 'Attendance Correction Request Successful.',
     data: request,
