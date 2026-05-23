8d121461d10bbeb55d18a40dd6fad85390f1f6b5
Harsh <devharshsalvi@gmail.com>
2026-05-23 16:34:30 +0530

    feat(event): added a new layer programmes

---
+++ src/database/programmes.model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose';
+
+const programmeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    workshops: {
+      type: [Schema.Types.ObjectId],
+      ref: 'Workshop',
+      default: [],
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+export type ProgrammeType = mongoose.InferSchemaType<typeof programmeSchema>;
+export const Programme = mongoose.model<ProgrammeType>('Programme', programmeSchema);
---
+++ src/events/programmes/programmes.schema.ts
@@ -0,0 +1,22 @@
+import { Types } from 'mongoose';
+import { z } from 'zod';
+
+const objectId = z.string().refine((val) => Types.ObjectId.isValid(val), {
+  message: 'Invalid ObjectId',
+});
+
+//Base programme schema
+export const baseProgrammeSchema = z.object({
+  title: z.string().min(5).max(50),
+  description: z.string().min(10).max(500),
+  workshops: z.array(objectId).optional(),
+});
+
+//Create a programme
+export const createProgrammeSchema = baseProgrammeSchema;
+
+//Update a programme
+export const updatedProgrammeSchema = baseProgrammeSchema.omit({ workshops: true }).partial();
+
+export type CreateProgrammeInputType = z.infer<typeof createProgrammeSchema>;
+export type UpdateProgrammeInputType = z.infer<typeof updatedProgrammeSchema>;
